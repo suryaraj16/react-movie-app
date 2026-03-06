8c86f9a69054f79d22856d66e10de9c3d857d9f3
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-06 20:12:43 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import MovieCard from "./components/MovieCard";
+import { fetchMovies } from "./services/api";
+import "./App.css";
+
+function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const data = await fetchMovies();
+      setMovies(data);
+    };
+
+    getMovies();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Popular Movies</h1>
+
+      <div className="movie-grid">
+        {movies.map((movie) => (
+          <MovieCard key={movie.imdbID} movie={movie} />
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
